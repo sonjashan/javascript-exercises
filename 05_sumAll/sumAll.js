cc0d95f645e0c41d4988d90336b92e0cfeb40455
sonjashan <102500611+sonjashan@users.noreply.github.com>
2024-10-26 16:59:59 -0400

    complete ex 5 on datatype and for loop

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+    // if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    if (typeof a !== 'number' || typeof b != 'number' || a <= 0 || b <= 0) return 'ERROR';
+    if (a !== Math.floor(a) || b !== Math.floor(b)) return 'ERROR';
+    let result = 0;
+    let more = b > a ? b : a;
+    let less = more === b ? a : b;
+    for (let i = less; i <= more; i++) result += i;
+    return result;
 };
 
 // Do not edit below this line
