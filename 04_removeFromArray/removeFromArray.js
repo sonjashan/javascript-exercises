2801494d723cb0840ee9865656af9bc2c6610d0b
sonjashan <102500611+sonjashan@users.noreply.github.com>
2024-10-25 21:15:27 -0400

    write fns as params & take undefined num of params

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+// const removeFromArray = function (arr, ...targets) {
+//     for (let t of targets) {
+//         let idx = arr.indexOf(t);
+//         while (idx !== -1) {
+//             arr.splice(idx, 1);
+//             idx = arr.indexOf(t);
+//         }
+//     }
+//     return arr;
+// };
+
+const removeFromArray = function (arr, ...targets) {
+    // return arr.filter(function (item) { return !targets.includes(item); });
+    return arr.filter(item => !targets.includes(item));
 };
 
 // Do not edit below this line
