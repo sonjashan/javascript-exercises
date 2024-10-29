0b76f87ea324d06e1609593c1556be4d5f21f605
sonjashan <102500611+sonjashan@users.noreply.github.com>
2024-10-29 17:20:55 -0400

    finish exercise on rounding

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,10 @@
-const convertToCelsius = function() {
+const ROUNDING = 10;
+const convertToCelsius = function (fah) {
+  return Math.round((fah - 32) * 5 / 9 * ROUNDING) / ROUNDING;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (cel) {
+  return Math.round((cel * 9 / 5 + 32) * ROUNDING) / ROUNDING;
 };
 
 // Do not edit below this line
